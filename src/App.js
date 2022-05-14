dfa726e8207d3a21c73579134562891646413709
Monika Mona <mona.mt.89@gmail.com>
2022-05-14 10:54:56 +0200

    add shop contact join path

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// DZIAŁA ROUTER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 import React from 'react'
 //import logo from './logo.svg'
 import './App.css'
@@ -10,6 +11,10 @@ const Home = () => <div>Home</div>
 const About = () => <div>About</div>
 const User = () => <div>User</div>
 const MyApp = () => <div>MyApp</div>
+const Shop = () => <div>Shop</div>
+const Free = () => <div>Free</div>
+const Contact = () => <div>Contact</div>
+const Join = () => <div>Join</div>
 
 function App() {
   return (
@@ -24,6 +29,10 @@ function App() {
               <Route path="/about" element={<About></About>}></Route>
               <Route path="/user" element={<User></User>}></Route>
               <Route path="/myapp" element={<MyApp></MyApp>}></Route>
+              <Route path="/shop" element={<Shop></Shop>}></Route>
+              <Route path="/free" element={<Free></Free>}></Route>
+              <Route path="/contact" element={<Contact></Contact>}></Route>
+              <Route path="/join" element={<Join></Join>}></Route>
             </Routes>
           </Router>
           <MainSection></MainSection>
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 function Navigation() {
   return (
     <div className="navigation">
       <nav>
-        <ul>
-          <li>Logo</li>
-          <li>about</li>
-          <li>shop</li>
-          <li>free</li>
-          <li>contact</li>
-          <li>join</li>
-        </ul>
+        <Router>
+          <ul>
+            <li>Logo</li>
+            <li>
+              <Link to="/about"></Link>about
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link></li>
+            <li>
+              <Link to="/free">free</Link></li>
+            <li>
+              <Link to="/contact">contact</Link></li>
+            <li>
+              <Link to="/join">join</Link></li>
+          </ul>
+        </Router>
       </nav>
     </div>
   )
